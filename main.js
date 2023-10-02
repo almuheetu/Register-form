ebe0ae49042374f1352651c2c3c136edc8ded12d
Almuheetu Shihab <almuheetu@gmail.com>
2023-10-02 19:50:25 +0600

    fixed

--- main.js
+++ main.js
@@ -4,10 +4,8 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     // Retrieve form values
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-    const rememberMe = document.getElementById("rememberMe").checked;
 
     // Perform your login or validation logic here
     console.log("Username:", username);
     console.log("Password:", password);
-    console.log("Remember Me:", rememberMe);
 });
